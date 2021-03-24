0bdfe0de6260d6d6d119ef146f8943d9f15bfdf2
Till Wehowski <github@webfan.de>
2021-03-24 20:36:20 +0100

    require.url...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
-
 exports.extensions =require('./lib/require-extensions');
 exports.web = require('./lib/from-web');
+exports.url = require('require-from-url');
+exports.urlSync = require('require-from-url/sync');
 exports.string = require('./lib/from-string');
